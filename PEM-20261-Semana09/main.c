822b9b282bc82e6d4894ad3262584f5c34dcc265
Pedro <138580994+Pedrowsl@users.noreply.github.com>
2026-05-15 14:55:38 -0300

    cabeçario-main.c

--- PEM-20261-Semana09/main.c
+++ PEM-20261-Semana09/main.c
@@ -1,3 +1,14 @@
+/*------------------------------------------------------------*
+*                      FATEC Ipiranga                         *
+*       Disciplina: Programação Estruturada e Modular         *
+*                   Prof. Carlos Veríssimo                    *
+*-------------------------------------------------------------*
+* Objetivo do Programa: Controlar o fluxo principal           *
+* do sistema e iniciar o menu da aplicação CUCC.              *
+* Data - 15/05/2026                                           *
+* Autor: Pedro Wesley                                         *
+*-------------------------------------------------------------*/
+
 #include "interface.h"
 
 int main()
