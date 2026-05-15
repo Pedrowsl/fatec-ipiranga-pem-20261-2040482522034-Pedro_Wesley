f4da8b70540d2a5dc2316dba5d888a9541e6b59f
Pedro <138580994+Pedrowsl@users.noreply.github.com>
2026-05-15 14:56:51 -0300

    cabeçario-recursao.h

--- PEM-20261-Semana09/recursao.h
+++ PEM-20261-Semana09/recursao.h
@@ -1,3 +1,14 @@
+/*------------------------------------------------------------*
+*                      FATEC Ipiranga                         *
+*       Disciplina: Programação Estruturada e Modular         *
+*                   Prof. Carlos Veríssimo                    *
+*-------------------------------------------------------------*
+* Objetivo do Programa: Declarar os protótipos das            *
+* funções recursivas utilizadas pelo sistema.                 *
+* Data - 15/05/2026                                           *
+* Autor: Pedro Wesley                                         *
+*-------------------------------------------------------------*/
+
 #ifndef RECURSAO_H
 #define RECURSAO_H
 
