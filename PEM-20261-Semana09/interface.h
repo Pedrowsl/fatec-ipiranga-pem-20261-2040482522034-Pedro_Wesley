572f893cf25cfeb1a86dbdc950454b1d04076a4b
Pedro <138580994+Pedrowsl@users.noreply.github.com>
2026-05-15 14:55:11 -0300

    cabeçario-interface.h

--- PEM-20261-Semana09/interface.h
+++ PEM-20261-Semana09/interface.h
@@ -1,3 +1,14 @@
+/*------------------------------------------------------------*
+*                      FATEC Ipiranga                         *
+*       Disciplina: Programação Estruturada e Modular         *
+*                   Prof. Carlos Veríssimo                    *
+*-------------------------------------------------------------*
+* Objetivo do Programa: Definir os protótipos das             *
+* funções responsáveis pela interface do usuário.             *
+* Data - 15/05/2026                                           *
+* Autor: Pedro Wesley                                         *
+*-------------------------------------------------------------*/
+
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
